2fac219dd68b86a8df5faf7715f2e423a1a75dd2
Julia Paluch <paluchj4@gmail.com>
2025-03-02 18:01:58 -0500

    display stations

--- src/c/metro-timetables.c
+++ src/c/metro-timetables.c
@@ -1,7 +1,50 @@
 #include <pebble.h>
 
+#define STATION_TEXT_GAP 14
+
 static Window *s_window;
 static TextLayer *s_text_layer;
+static MenuLayer *s_menu_layer;
+
+static char station_text[32];
+
+#ifdef PBL_ROUND
+static int16_t get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *cell_index, 
+                                        void *callback_context) {
+  return 60;
+}
+#endif
+
+typedef struct {
+  char name[32]; // Name of this station
+} Station;
+
+
+// Array of different stations
+// {<Station Name>, <Lines OR'd together>}
+Station station_array[] = {
+  {"Dupont"},
+  {"Court House"},
+  {"Rosslyn"},
+  {"Metro Center"},
+};
+
+static void draw_row_handler(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, 
+                             void *callback_context) {
+  char* name = station_array[cell_index->row].name;
+  int text_gap_size = STATION_TEXT_GAP - strlen(name);
+
+  // Using simple space padding between name and station_text for appearance of edge-alignment
+  snprintf(station_text, sizeof(station_text), "%s", PBL_IF_ROUND_ELSE("", name));
+  menu_cell_basic_draw(ctx, cell_layer, PBL_IF_ROUND_ELSE(name, station_text), 
+                       PBL_IF_ROUND_ELSE(station_text, NULL), NULL);
+}
+
+static uint16_t get_sections_count_callback(struct MenuLayer *menulayer, uint16_t section_index, 
+                                            void *callback_context) {
+  int count = sizeof(station_array) / sizeof(Station);
+  return count;
+}
 
 static void prv_select_click_handler(ClickRecognizerRef recognizer,
                                      void *context) {
@@ -36,6 +79,16 @@ static void prv_window_load(Window *window) {
   text_layer_set_text(s_text_layer, "Timetables");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  
+  s_menu_layer = menu_layer_create(bounds);
+  menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks){
+    .get_num_rows = get_sections_count_callback,
+    .get_cell_height = PBL_IF_ROUND_ELSE(get_cell_height_callback, NULL),
+    .draw_row = draw_row_handler,
+    .select_click = NULL
+  }); 
+  menu_layer_set_click_config_onto_window(s_menu_layer,	window);
+  layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
 }
 
 static void prv_window_unload(Window *window) {
